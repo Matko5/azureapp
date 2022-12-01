dd43c247236e3d332aa03d526558e2edd8e22de2
Martin <martin.roman@softpoint.sk>
2022-12-01 18:58:22 +0100

    requirements

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import os
 from io import BytesIO
+from azure.storage.blob import BlobServiceClient
 
 app = Flask(__name__)
 
@@ -16,5 +17,8 @@ def register():
 def task():
     connection_string = 'DefaultEndpointsProtocol=https;AccountName=mystorageaccount11917;AccountKey=b3QGLoNtwtkKKXzkwzXjTfj+7k4N7nk8IL8RVvdLMfK+AYOEnur/3QGFgIl50JyH35GqoXQmW0Tp+AStl21efg==;EndpointSuffix=core.windows.net'
     container_name = 'inovices'
+    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+    container_client = blob_service_client.get_container_client(container_name)
+    file = BytesIO()
     ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
     return ROOT_DIR
