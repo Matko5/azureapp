7f7fcf3356ffbe3821431d0f6bfd22a34a28b169
Martin <martin.roman@softpoint.sk>
2022-12-01 18:40:48 +0100

    requirements

--- app.py
+++ app.py
@@ -17,7 +17,5 @@ def register():
 def task():
     connection_string = 'DefaultEndpointsProtocol=https;AccountName=mystorageaccount11917;AccountKey=b3QGLoNtwtkKKXzkwzXjTfj+7k4N7nk8IL8RVvdLMfK+AYOEnur/3QGFgIl50JyH35GqoXQmW0Tp+AStl21efg==;EndpointSuffix=core.windows.net'
     container_name = 'inovices'
-    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-    container_client = blob_service_client.get_container_client(container_name)
     ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
     return ROOT_DIR
